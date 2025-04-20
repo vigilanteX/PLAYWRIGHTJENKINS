545b45c4322462b290d4671fa4e8836d42d4eb68
vigilanteX <dixitaman8@gmail.com>
2025-04-20 10:23:59 +0530

    xx

--- playwright.config.js
+++ playwright.config.js
@@ -38,17 +38,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    }
 
     /* Test against mobile viewports. */
     // {
